c90b00b70a8a0d39297116cb18dbc9ff004d4fff
Naokiakazawa <naoki9akazawa@gmail.com>
2023-11-16 21:36:02 +0900

    modify configs for monorepo

--- services/frontend/apps/mobile/metro.config.js
+++ services/frontend/apps/mobile/metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -7,10 +8,44 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-    resolver: {
-        unstable_enableSymlinks: true,
-        unstable_enablePackageExports: true,
+  watchFolders: [path.resolve(__dirname, '../../node_modules')],
+  resolver: {
+    unstable_enableSymlinks: true,
+    unstable_enablePackageExports: true,
+  },
+  transformer: {
+    /**
+     * モノレポでAndroidが静的ファイルを読み込めるようにするための設定
+     * Androidの画像のホスト先
+     */
+    publicPath: '/assets/dark/magic',
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+/**
+   * モノレポでAndroidが静的ファイルを読み込めるようにするための設定
+   * metroはnodeサーバを起動してエミュレーターを動かしている
+   * 画像のホスト先を変更し、特定のリクエストを書き換えることで読み込めるようになる
+   * assets/dark/magicはmetroがAndroidをビルドする時の画像の出力先になっている
+   */
+  server: {
+    enhanceMiddleware: middleware => {
+      return (req, res, next) => {
+        if (req.url.startsWith('/assets/dark/magic')) {
+          req.url = req.url.replace('/assets/dark/magic', '/assets');
+        } else if (req.url.startsWith('/assets/dark')) {
+          req.url = req.url.replace('/assets/dark', '/assets/..');
+        } else if (req.url.startsWith('/assets')) {
+          req.url = req.url.replace('/assets', '/assets/../..');
+        }
+        return middleware(req, res, next);
+      };
     },
+  },
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
