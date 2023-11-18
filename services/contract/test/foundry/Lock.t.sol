d1ade9277654775f45a82f44ace79abbe52e3e2f
Naokiakazawa <naoki9akazawa@gmail.com>
2023-11-18 15:20:56 +0900

    add test for foundry

---
+++ services/contract/test/foundry/Lock.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.9;
+
+import "forge-std/Test.sol";
+
+contract ContractTest is Test {
+    uint256 testNumber;
+
+    function setUp() public {
+    }
+
+    function test_NumberIs42() public {
+    }
+
+}
