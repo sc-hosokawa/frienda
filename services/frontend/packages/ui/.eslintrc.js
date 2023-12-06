030b23f3be474fce11646f4f8673e30299305877
Naokiakazawa <naoki9akazawa@gmail.com>
2023-12-06 17:39:39 +0900

    add shadcn

--- services/frontend/packages/ui/.eslintrc.js
+++ services/frontend/packages/ui/.eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ["custom/react-internal"],
+  env: {
+    "node": true
+  },
+  "rules": {
+    "import/no-extraneous-dependencies": "off",
+    "react/no-unknown-property": "off"
+  }
 };
