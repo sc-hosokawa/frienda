52cf3eb098bcd9fa62540a099d381a03376f7b74
Naokiakazawa <naoki9akazawa@gmail.com>
2024-09-28 21:52:53 +0900

    change host and port

--- services/backend/server-core/src/main.rs
+++ services/backend/server-core/src/main.rs
@@ -1,9 +1,14 @@
 use actix_web::{web, App, HttpServer};
 use infrastracture::handlers;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Starting server...");
+
+    let host = env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let port: u16 = port.parse().expect("PORT must be a number");
 
     HttpServer::new(|| {
         App::new().route(
@@ -11,7 +16,7 @@ async fn main() -> std::io::Result<()> {
             web::post().to(handlers::stripe_webhook::webhook_handler),
         )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((host, port))?
     .run()
     .await
 }
