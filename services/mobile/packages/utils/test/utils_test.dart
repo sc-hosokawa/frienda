d941a85931bab54d1d7a3a5fe8573fe393232f3f
Naokiakazawa <naoki9akazawa@gmail.com>
2024-10-12 12:33:42 +0900

    fix test on packages

--- services/mobile/packages/utils/test/utils_test.dart
+++ services/mobile/packages/utils/test/utils_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:utils/utils.dart';
+import 'package:utils/utils/utils.dart';
 
 void main() {
   test('adds one to input values', () {
