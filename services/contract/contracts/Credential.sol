f8f6aaf0c9b20b9ac6683e42a042557182f5c030
shuhei tanaka <shuhei46491@gmail.com>
2024-12-03 19:46:40 +0900

    add Natspec

--- services/contract/contracts/Credential.sol
+++ services/contract/contracts/Credential.sol
@@ -10,6 +10,9 @@ import {ERC20PausableUpgradeable} from
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
+/// @title Credential contract
+/// @notice ERC20 token representing a credential
+/// @dev The contract is upgradeable using the UUPS pattern
 contract Credential is
     Initializable,
     ERC20Upgradeable,
@@ -18,19 +21,33 @@ contract Credential is
     AccessControlUpgradeable,
     UUPSUpgradeable
 {
+    /// @notice Pauser role for pausing and unpausing the contract
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    /// @notice Minter role for minting new tokens
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /// @notice Burner role for burning tokens
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    /// @notice Upgrader role for upgrading the contract
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
     /// @notice Error message for invalid length
     error INVALID_LENGTH();
+
+    /// @notice Event emitted when a credential is granted
+    event CredentialGranted(address indexed account, uint256 amount);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
+    /// @notice Initialize the contract
+    /// @param initialOwner The address that will have the default admin/upgrader role
+    /// @param pauser The address that will have the pauser role
+    /// @param minter The address that will have the minter/burner role
     function initialize(address initialOwner, address pauser, address minter) public initializer {
         __ERC20_init("Credential", "CRED");
         __ERC20Burnable_init();
@@ -46,25 +63,37 @@ contract Credential is
 
     function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADER_ROLE) {}
 
+    /// @notice Pause the contract
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
+    /// @notice Unpause the contract
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
+    /// @notice Mint new tokens to the specified addresses
+    /// @dev The length of the `to` and `amount` arrays must be the same, otherwise the function will revert
+    /// @param to The addresses to which the tokens will be minted
+    /// @param amount The amount of tokens to mint to each address
     function mint(address[] memory to, uint256[] memory amount) public onlyRole(MINTER_ROLE) {
         require(to.length == amount.length, INVALID_LENGTH());
         for (uint256 i = 0; i < to.length; i++) {
             _mint(to[i], amount[i]);
+            emit CredentialGranted(to[i], amount[i]);
         }
     }
 
+    /// @notice Burn tokens from the specified addresses
+    /// @dev The length of the `from` and `amount` arrays must be the same, otherwise the function will revert
+    /// @dev from address must have allowance for the burner to burn the tokens
+    /// @param from The addresses from which the tokens will be burned
+    /// @param amount The amount of tokens to burn from each address
     function burn(address[] memory from, uint256[] memory amount) public onlyRole(BURNER_ROLE) {
         require(from.length == amount.length, INVALID_LENGTH());
         for (uint256 i = 0; i < from.length; i++) {
-            _burn(from[i], amount[i]);
+            burnFrom(from[i], amount[i]);
         }
     }
 
