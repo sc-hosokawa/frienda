1e294bb9b34909afe2a8b75fefaf9136b94a0f2e
Naokiakazawa <naoki9akazawa@gmail.com>
2024-12-15 22:45:46 +0900

    fix fcm setup logic

--- services/backend/server-core/infrastracture/src/services/fcm.rs
+++ services/backend/server-core/infrastracture/src/services/fcm.rs
@@ -1,4 +1,6 @@
 use async_trait::async_trait;
+use std::env;
+use tempfile;
 
 use application::services::push_notification::PushNotificationServiceTrait;
 use domain::services::notification::PushNotification;
@@ -14,8 +16,11 @@ pub struct FcmNotificationService {
 impl FcmNotificationService {
     pub async fn new() -> Result<Self, anyhow::Error> {
         tracing::info!("Setting up FcmNotificationService...");
-        let service_account_path = "frienda-auth-test1-a490c287d01b.json";
-        let client = FcmClient::new(service_account_path).await?;
+        let service_account_json = env::var("FCM_SERVICE_ACCOUNT")?;
+        let temp_file = tempfile::NamedTempFile::new()?;
+        println!("temp_file: {:?}", temp_file);
+        std::fs::write(&temp_file, service_account_json)?;
+        let client = FcmClient::new(temp_file.path().to_str().unwrap()).await?;
         Ok(Self { client })
     }
 }
