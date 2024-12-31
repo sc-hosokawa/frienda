65a085011de236c0d02938d5debe34469c8e42c1
Naokiakazawa <naoki9akazawa@gmail.com>
2024-12-31 11:36:59 +0900

    fix layout

--- services/webui/apps/client/app/(walkthrough)/layout.tsx
+++ services/webui/apps/client/app/(walkthrough)/layout.tsx
@@ -11,7 +11,7 @@ const JostFont = Jost({
   variable: "--font-jost",
 });
 
-export const NotoSansJP = Noto_Sans_JP({
+const NotoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
   weight: ["100", "300", "400", "500", "700", "900"],
   display: "swap",
