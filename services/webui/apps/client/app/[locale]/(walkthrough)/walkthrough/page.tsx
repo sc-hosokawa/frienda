e9dc9f0b0891c202ed1435d904da548c36e589ca
shuhei tanaka <shuhei46491@gmail.com>
2025-01-17 16:14:13 +0900

    feat(workthrough): apply translation

--- services/webui/apps/client/app/[locale]/(walkthrough)/walkthrough/page.tsx
+++ services/webui/apps/client/app/[locale]/(walkthrough)/walkthrough/page.tsx
@@ -3,41 +3,38 @@
 import Image from "next/image";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { useTranslation } from "~/i18n/client";
 
 const Walkthrough = () => {
   const [step, setStep] = useState(0);
   const router = useRouter();
+  const { t } = useTranslation();
 
   const slides = [
     {
       image: "/tutorial/tutorial-welcome.gif",
       title: "Welcome to FRIENDSHIP. DAO",
-      description:
-        "FRIENDSHIP.DAOへようこそ！\n\n簡単にFRIENDSHIP.DAOでできることを説明します。",
+      description: t("workthrough.welcome"),
     },
     {
       image: "/tutorial/tutorial-services.gif",
       title: "HOME",
-      description:
-        "Homeでは、OffersやDashboardのサマリーを確認できます。また、あなたの活動や音楽の視聴数などに応じて、アクションの提案をおすすめします。",
+      description: t("workthrough.home"),
     },
     {
       image: "/tutorial/tutorial-offer.gif",
       title: "OFFERS",
-      description:
-        "Offersでは、あなたにあったオファーを探すことができ、オファーに応募して承認されると、オファー作成者やオファーのパートナーと共に、オファーを始めることになります。オファーを完了すると、ポイントが付与されます。",
+      description: t("workthrough.offers"),
     },
     {
       image: "/tutorial/tutorial-dashboard.gif",
       title: "DASHBOARD",
-      description:
-        "Dashboardでは、登録された音楽チャンネルの再生数を統合し、あなたのアーティストとしての活動を可視化します。週、月での再生数の確認、再生されているユーザーや国の内訳などあなたにとって有益な情報が確認できます。",
+      description: t("workthrough.dashboard"),
     },
     {
       image: "/tutorial/tutorial-community.gif",
       title: "COMMUNITY",
-      description:
-        "Communityでは、繋がった他のユーザーとの繋がりを確認できます。また、繋がったユーザーのユーザーページを確認し、そのユーザーの繋がりを確認することもできます。気になったユーザーがいたら、メッセージを送って繋がりましょう。",
+      description: t("workthrough.community"),
     },
   ];
 
@@ -46,7 +43,7 @@ const Walkthrough = () => {
       setStep(step + 1);
     } else {
       localStorage.setItem("walkthroughCompleted", "true");
-      router.push("/");
+      router.push("/home");
     }
   };
 
@@ -67,10 +64,10 @@ const Walkthrough = () => {
           <div className="w-full flex justify-end">
             {step < slides.length - 1 && (
               <button
-                onClick={() => router.push("/")}
+                onClick={() => router.push("/home")}
                 className="text-[14px] font-light decoration-dashed underline"
               >
-                スキップする
+                {t("common.skip")}
               </button>
             )}
           </div>
@@ -92,7 +89,7 @@ const Walkthrough = () => {
                   onClick={() => setStep(step - 1)}
                   className="text-black bg-white border-2 border-[#E4DBC0] hover:bg-gray-50 w-[180px] h-[60px] text-[14px] rounded-full"
                 >
-                  戻る
+                  {t("common.back")}
                 </button>
               )}
               <button
@@ -103,7 +100,9 @@ const Walkthrough = () => {
                     : "w-[180px] h-[60px] text-[14px]"
                 }`}
               >
-                {step < slides.length - 1 ? "次へ" : "はじめる"}
+                {step < slides.length - 1
+                  ? t("common.next")
+                  : t("common.start")}
               </button>
             </div>
           </div>
