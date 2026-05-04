3c90863925e46f7ffbeb06acda659a22ebbf2966
hosokawa <sc-hosokawa@users.noreply.github.com>
2026-05-04 09:00:26 +0900

    feat(db): add NULL play_count count queries for all DSP daily tables

--- services/postgres/seeds/13_count_plays_dsp_daily.sql
+++ services/postgres/seeds/13_count_plays_dsp_daily.sql
@@ -24,3 +24,29 @@ SELECT 'plays_apple_country_daily', COUNT(1) FROM "plays_apple_country_daily"
 UNION ALL
 SELECT 'plays_amazon_country_daily', COUNT(1) FROM "plays_amazon_country_daily"
 ;
+SELECT 'plays_shazam_daily', COUNT(1) FROM "plays_shazam_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_youtube_age_daily', COUNT(1) FROM "plays_youtube_age_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_youtube_gender_daily', COUNT(1) FROM "plays_youtube_gender_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_youtube_country_daily', COUNT(1) FROM "plays_youtube_country_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_spotify_age_daily', COUNT(1) FROM "plays_spotify_age_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_spotify_gender_daily', COUNT(1) FROM "plays_spotify_gender_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_spotify_country_daily', COUNT(1) FROM "plays_spotify_country_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_line_age_daily', COUNT(1) FROM "plays_line_age_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_line_gender_daily', COUNT(1) FROM "plays_line_gender_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_apple_age_daily', COUNT(1) FROM "plays_apple_age_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_apple_gender_daily', COUNT(1) FROM "plays_apple_gender_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_apple_country_daily', COUNT(1) FROM "plays_apple_country_daily" WHERE play_count IS NULL
+UNION ALL
+SELECT 'plays_amazon_country_daily', COUNT(1) FROM "plays_amazon_country_daily" WHERE play_count IS NULL
+;
